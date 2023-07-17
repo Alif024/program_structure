c75189bc9c7f1e6bf8345d59c5ac600f8c3f6feb
Alif024 <aleefrock12345@gmail.com>
2023-07-18 05:38:15 +0700

    #

--- lab8/no_4.py
+++ lab8/no_4.py
@@ -1,16 +1,13 @@
 import os
-os.system('cls')
-def home():
+list_data = []
+while True:
+    os.system('cls')
     print("="*20, " BMI Calculator ", "="*17)
     print("1. Calculation")
     print("2. Show data")
     print("3. Exit program")
     print("="*55)
-home()
-mode = input("Please choose 1-3:")
-os.system('cls')
-list_data = []
-while True:
+    mode = input("Please choose 1-3:")
     match mode:
         case "1":
             mode1 = True
@@ -49,18 +46,15 @@ while True:
             except KeyboardInterrupt:
                 mode1 = False
         case "2":
-            mode2 = True
-            while mode2:
-                os.system('cls')
-                print("="*20, " BMI Detail ", "="*32)
-                print("No.\tHeight\t\tWeight\t\tBMI\tDetail")
-                print("*"*66)
-                for i in range(len(list_data)):
-                    print(f"{i+1}\t{list_data[i][1]:.2f}\t\t{list_data[i][2]:.2f}\t\t{list_data[i][3]:.2f}\t{list_data[i][4]}")
-                print("="*66)
-                print("กลับหน้าหลัก Press Enter")
-                input("")
-                mode2 = False
+            os.system('cls')
+            print("="*20, " BMI Detail ", "="*32)
+            print("No.\tHeight\t\tWeight\t\tBMI\tDetail")
+            print("*"*66)
+            for i in range(len(list_data)):
+                print(f"{i+1}\t{list_data[i][1]:.2f}\t\t{list_data[i][2]:.2f}\t\t{list_data[i][3]:.2f}\t{list_data[i][4]}")
+            print("="*66)
+            print("กลับหน้าหลัก Press Enter")
+            input("")
         case "3":
             os.system('cls')
             confirm = input("Do you want to exit BMI Calculator (y/[n])? ")
@@ -71,6 +65,3 @@ while True:
                 pass
         case _:
             pass
-    os.system('cls')
-    home()
-    mode = input("Please choose 1-3:")  
