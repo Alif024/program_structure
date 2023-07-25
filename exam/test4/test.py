7b187113bccf89d54a1623c2e2e1a4e2d84ed3c8
Alif024 <aleefrock12345@gmail.com>
2023-07-25 11:33:37 +0700

    #

--- exam/test4/test.py
+++ exam/test4/test.py
@@ -1,21 +1,23 @@
 data = {}
 try:
     while True:
-        print("""เลือกการทำงานของโปรแกรม
+        print("""การทำงานของโปรแกรม
     1 : ใส่ข้อมูลพนักงาน
     2 : แสดงชื่อพนักงานทั้งหมด
     3 : แสดงข้อมูลของพนักงาน
-""")
-        id = int(input("Enter ID >> "))
-        name = input("Enter name >> ")
-        summit = int(input("Enter summit >> "))
-        if summit > 50000:
-            bonus = summit + summit*30/100
-        else:
-            bonus = summit + summit*10/100
-        if id == "":
-            break
-        data[id] = {"name": f"{name}", "summit": summit, "bonus": bonus}
+""")    
+        mode = input("เลือกโหมด")
+        match mode:
+            case "1":
+                while True:
+                    id = int(input("Enter ID >> "))
+                    name = input("Enter name >> ")
+                    summit = int(input("Enter summit >> "))
+                    if summit > 50000:
+                        bonus = summit + summit*30/100
+                    else:
+                        bonus = summit + summit*10/100
+                    data[id] = {"name": f"{name}", "summit": summit, "bonus": bonus}
 except :
     pass
     print(data)
