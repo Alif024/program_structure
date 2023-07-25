af884721494beb40afd8dfc344e1d13fbe173c2f
Alif024 <aleefrock12345@gmail.com>
2023-07-25 09:41:30 +0700

    Update no3.py

--- exam/no3.py
+++ exam/no3.py
@@ -1,8 +1,14 @@
 num = int(input("Please enter number of factorial : "))
-result = 1
-for i in range(0, num+1):
-    if i == 0:
-        pass
+while True:
+    if num < 10 and num > 0:
+        result = 1
+        for i in range(0, num+1):
+            if i == 0:
+                pass
+            else:
+                result = result * i
+        print(f"Result of {num}! is {result}")
+        break
     else:
-        result = result * i
-print(f"Result of {num}! is {result}")
+        print("ใส่แค่ 1-9")
+        num = int(input("Please enter number of factorial : "))
