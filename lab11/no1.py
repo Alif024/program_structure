6af16cf24c845014f1d6e9367b518ef7395ab52f
Alif024 <aleefrock12345@gmail.com>
2023-08-22 09:39:32 +0700

    #

--- lab11/no1.py
+++ lab11/no1.py
@@ -1,6 +1,13 @@
 from random import randint
 
 
+def random_num():
+    rdNum = randint(0, 1000000)
+    print(f"{rdNum:,}")
+    split_num = list(str(rdNum))
+    return [int(i) for i in split_num]
+
+
 def even_num(listNum):
     return [even_num for even_num in listNum if even_num % 2 == 0]
 
@@ -9,10 +16,7 @@ def odd_num(listNum):
     return [odd_num for odd_num in listNum if odd_num % 2 != 0]
 
 
-rdNum = randint(0, 1000000)
-print(f"{rdNum:,}")
-split_num = list(str(rdNum))
-listNum = [int(i) for i in split_num]
+listNum = random_num()
 print(f"sum = {sum(listNum)}")
 print(f"{len(even_num(listNum))} ตัว {even_num(listNum)}")
 print(f"{len(odd_num(listNum))} ตัว {odd_num(listNum)}")
